e033e8874e2ad47a7dea9ea1cde36bb6efc141ba
kvbwork <kvbdev@yandex.ru>
2022-10-16 21:51:53 +0500

    findByNumber implemented

--- src/main/java/kvbdev/PhoneBook.java
+++ src/main/java/kvbdev/PhoneBook.java
@@ -2,6 +2,7 @@ package kvbdev;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class PhoneBook {
 
@@ -24,18 +25,24 @@ public class PhoneBook {
     }
 
     private final Map<String, Contact> contactNameIndexMap = new HashMap<>();
+    private final Map<String, Contact> contactPhoneIndexMap = new HashMap<>();
 
     public int add(String name, String phone) {
         if (name == null || name.isEmpty()) return contactNameIndexMap.size();
         if (phone == null || phone.isEmpty()) return contactNameIndexMap.size();
+        if (contactNameIndexMap.containsKey(name)) return contactNameIndexMap.size();
 
         Contact newContact = new Contact(name, phone);
-        contactNameIndexMap.putIfAbsent(name, newContact);
+        contactNameIndexMap.put(name, newContact);
+        contactPhoneIndexMap.put(phone, newContact);
 
         return contactNameIndexMap.size();
     }
 
     public String findByNumber(String phoneNumber) {
-        return null;
+        if (phoneNumber == null || phoneNumber.isEmpty()) return "";
+        return Optional.ofNullable(contactPhoneIndexMap.get(phoneNumber))
+                .map(Contact::getName)
+                .orElse("");
     }
 }
