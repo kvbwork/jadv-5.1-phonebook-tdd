22de57010e7717e783016a872b9d9acc76ef37bd
kvbwork <kvbdev@yandex.ru>
2022-10-16 21:16:12 +0500

    add implemented

--- src/main/java/kvbdev/PhoneBook.java
+++ src/main/java/kvbdev/PhoneBook.java
@@ -1,8 +1,37 @@
 package kvbdev;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
-    public int add(String name, String phone){
-        return -1;
+    protected static class Contact {
+        private final String name;
+        private final String phone;
+
+        public Contact(String name, String phone) {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+    }
+
+    private final Map<String, Contact> contactNameIndexMap = new HashMap<>();
+
+    public int add(String name, String phone) {
+        if (name == null || name.isEmpty()) return contactNameIndexMap.size();
+        if (phone == null || phone.isEmpty()) return contactNameIndexMap.size();
+
+        Contact newContact = new Contact(name, phone);
+        contactNameIndexMap.putIfAbsent(name, newContact);
+
+        return contactNameIndexMap.size();
     }
 }
